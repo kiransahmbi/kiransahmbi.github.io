acfa6b02024b083ea606c6beea2d9235c6e5729c
Kiran <kiransahmbi@yahoo.ca>
2022-05-06 23:29:38 -0400

    Added animations

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Change nav-bar colour
 $(function () {
     $(document).scroll(function () {
       var $nav = $(".navbar");
@@ -6,3 +8,53 @@ $(function () {
       $navtext.toggleClass('scrolled-text', $(this).scrollTop() > $nav.height());
     });
   });
+
+// About Me Moving
+const toObserveAboutMe = document.querySelectorAll(".to-observe-about-me");
+
+let options = {
+    rootMargin: '0px',
+    threshold: [0.5, 1]
+  }
+  
+const observerAboutMe = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("showAboutMe", entry.isIntersecting)
+            if (entry.isIntersecting) observerAboutMe.unobserve(entry.target)
+        })
+    }, options);
+
+toObserveAboutMe.forEach(item => {
+    observerAboutMe.observe(item)
+})
+
+// Header Moving
+const toObserveHeader = document.querySelectorAll(".header .row");
+
+const observerHeader = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("showHeader", entry.isIntersecting)
+            if (entry.isIntersecting) observerHeader.unobserve(entry.target)
+        })
+    });
+
+toObserveHeader.forEach(item => {
+    observerHeader.observe(item)
+})
+
+// Projects Moving
+const toObserveProjects = document.querySelectorAll("#projects .container");
+  
+const observerProjects = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("showProjects", entry.isIntersecting)
+            if (entry.isIntersecting) observerProjects.unobserve(entry.target)
+        })
+    }, {threshold: 0.20});
+
+toObserveProjects.forEach(item => {
+    observerProjects.observe(item)
+})
